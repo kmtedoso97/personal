4d6d3a6f93f6fe9ab454200c865fb407d5f2ae41
Krisha Marie Tedoso <krishatedoso@gmail.com>
2020-11-29 11:30:39 +0800

    Add files via upload

---
+++ cart/script.js
@@ -0,0 +1,23 @@
+var noti = document.querySelector('h1');
+var select = document.querySelector('.select');
+var button = document.getElementsByTagName('button');
+for(but of button)
+{
+    but.addEventListener('click', (e)=>{
+        var add = Number(noti.getAttribute('data-count')|| 0);
+        noti.setAttribute('data-count', add + 1);
+        noti.classList.add('zero');
+
+        //copy and paste elemet//
+        var parent = e.target.parentNode;
+        var clone = parent.cloneNode(true);
+        select.appendChild(clone);
+        clone.lastElementChild.innerText = "Buy-now";
+        if (clone) {
+            noti.onclick =()=>{
+                select.classList.toggle('display');
+            }
+        }
+
+        })
+    }
